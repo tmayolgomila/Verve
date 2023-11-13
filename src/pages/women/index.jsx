8b3fd8c65a717507c30778ff8e989712d927e1a7
Tomeu <tomeu.mayol@etechyachting.com>
2023-11-13 15:58:22 +0100

    ADD styles

--- src/pages/women/index.jsx
+++ src/pages/women/index.jsx
@@ -1,22 +1,24 @@
 import GalleryCard from "../../components/galleryCard"
+import "./styles.css"
+import Impro1 from "../../img/impro1.jpeg"
+import Impro2 from "../../img/impro2.jpeg"
+import Impro3 from "../../img/impro3.webp"
 
 export const Women = () => {
 
   return (
-    
-    <div className="gallery">
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
-        <GalleryCard/>
+    <div className="galleryContainer">
+      <div className="gallery">
+          <GalleryCard galleryImg={Impro1} galleryTitle="one Title" galleryButton="Add to cart"/>
+          <GalleryCard galleryImg={Impro3} galleryTitle="screen cleaner title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro1} galleryTitle="single line title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro3} galleryTitle="tanks title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro1} galleryTitle="user captain" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro3} galleryTitle="triangle title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro1} galleryTitle="gallery title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro3} galleryTitle="user worker title" galleryButton="Add to cart" />
+          <GalleryCard galleryImg={Impro1} galleryTitle="various title system" galleryButton="Add to cart" />
+      </div>
     </div>
 
   )
